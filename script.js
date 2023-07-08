605495a7003f11b34b4b0350a17e5f4e0f4717b9
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-07-08 01:08:32 -0400

    Add basic validation

---
+++ script.js
@@ -0,0 +1,19 @@
+let form = document.querySelector("form");
+let submitButton = document.querySelector("button.sign-up");
+let firstName = document.querySelector("#first-name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#pwd-1");
+let passwordConfirm = document.querySelector("#pwd-2");
+
+form.addEventListener("mouseover", validateForms);
+submitButton.addEventListener("mouseover", validateForms);
+
+function validateForms() {
+	if (firstName.required === false) {
+		firstName.required = true;
+		email.required = true;
+		password.required = true;
+		passwordConfirm.required = true;
+		submitButton.type = "submit";
+	}
+}
